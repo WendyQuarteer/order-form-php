f7de87e9fbe72524f2f9963ffb497efab48471cf
Wendy Quarteer <wendyquarteer@outlook.com>
2022-06-20 15:16:38 +0200

    setup repository + preparation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # order-form-php
+
+## Preparation
+- [x] Have a look at the provided structure: you get both an index file and another file containing a form. 
+How are these two working together?
+  Index.php is about creating objects that contain both data and functions that will be used in the form-view.php file.
+- [x] Think of a funny / surprising / original name for a store that should definitely exist. (fancy suits for cats? 
+- bongo for dates? you name it!)
+  The Hairy-Fish-Store
+- [x] Think of some products to sell (feel free to be creative) and update the products array with these.
+  fish-wax, fish-comb, fish-shampoo, fish-blow-dryer, fish-fake-pony-tail
+- [x] Check if all the products & prices are currently visible in the form.
+
+## Step 1: accepting orders
+Show an order confirmation when the user submits the form. This should contain the chosen products and delivery address.
+We will learn how to save this information to a database later, so no need to do this now.
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+// This file is your starting point (= since it's the index)
+// It will contain most of the logic, to prevent making a messy mix in the html
+
+// This line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+// We are going to use session variables, so we need to enable sessions
+session_start();
+
+// Use this function when you need to need an overview of these variables
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Fish-bathtub', 'price' => 45.80],
+    ['name' => 'Fish-shampoo', 'price' => 12.50],
+    ['name' => 'fish-comb', 'price' => 7.00],
+    ['name' => 'fish-blow-dryer', 'price' => 35.00],
+    ['name' => 'fish-wax', 'price' => 8.25],
+    ['name' => 'fish-fake-pony-tail', 'price' => 21.15]
+];
+
+$totalValue = 0;
+
+function validate()
+{
+    // TODO: This function will send a list of invalid fields back
+    return [];
+}
+
+function handleForm()
+{
+    // TODO: form related tasks (step 1)
+
+    // Validation (step 2)
+    $invalidFields = validate();
+    if (!empty($invalidFields)) {
+        // TODO: handle errors
+    } else {
+        // TODO: handle successful submission
+    }
+}
+
+// TODO: replace this if by an actual check
+$formSubmitted = false;
+if ($formSubmitted) {
+    handleForm();
+}
+
+require 'form-view.php';
